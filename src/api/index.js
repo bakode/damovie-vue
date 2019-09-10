c1a6a65b640e2af4f5ee227e20ae61211eec622a
aasumitro <aasumitro@gmail.com>
2019-09-10 13:55:10 +0800

    update api

--- src/api/index.js
+++ src/api/index.js
@@ -2,19 +2,19 @@ import axios from 'axios'
 
 const API_URL = process.env.VUE_APP_API_URL
 const API_KEY = process.env.VUE_APP_API_KEY
-const SORT_BY = 'popularity.desc'
+
+function api () {
+    return axios.create({
+        baseURL: API_URL
+    })
+}
 
 export function fetchMovies(page) {
-    var url =  API_URL+'discover/movie?'
-                +"api_key="+API_KEY
-                +'&page='+page
-                +'&sort_by='+SORT_BY
-    return axios.get(url)
+    var uri = 'discover/movie?api_key='+API_KEY+'&page='+page+'&sort_by=popularity.desc'
+    return api().get(uri)
 }
 
 export function searchMovie(param) {
-    var url =  API_URL+'search/movie?'
-    +"api_key="+API_KEY
-    +'&query='+param
-    return axios.get(url)
+    var uri = 'search/movie?api_key='+API_KEY+'&query='+param
+    return api().get(uri)
 }
